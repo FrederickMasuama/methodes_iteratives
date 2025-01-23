6b64122db534e071f6bdc118073bd1c8d2a529c0
FrédérickMAS <frederickmasuama@gmail.com>
2025-01-23 16:24:54 +0100

    premiers jets

--- methodes_iteratives.py
+++ methodes_iteratives.py
@@ -1,2 +1,12 @@
 import numpy as np
 import matplotlib.pyplot as plt
+
+A = np.array([[10, 2, 1],
+     [2, 10, 3],
+     [1, 3, 10]])
+
+b_1 = np.array([[15, 29, 27]])#sans oublier les doubles crochets
+b = b_1.T #transposée du vecteur
+
+print (A)
+print (b)
