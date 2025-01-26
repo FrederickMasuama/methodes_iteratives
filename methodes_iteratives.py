1f3fd56d63ccdb1535b0abf146be143feb9e7e16
FrédérickMAS <frederickmasuama@gmail.com>
2025-01-26 11:47:29 +0100

    implémentation de Jacobi et Gauss-Seidel

--- methodes_iteratives.py
+++ methodes_iteratives.py
@@ -1,12 +1,78 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-A = np.array([[10, 2, 1],
+#Matrices
+A = np.array([[10, 2, 1], #Matrice symétrique à diagonale fortement dominant
      [2, 10, 3],
      [1, 3, 10]])
 
 b_1 = np.array([[15, 29, 27]])#sans oublier les doubles crochets
 b = b_1.T #transposée du vecteur
 
-print (A)
-print (b)
+# Données
+max_iter = 1000
+tol = 1e-6
+x = np.zeros_like(b , dtype = float)
+
+# Méthode de Jacobi
+def jacobi(A, b, x, max_iter, tol):
+    M = np.diag(np.diag(A))
+    N = - ( np.tril(A, k=-1) + np.triu(A, k=1) )
+    M_inv = np.linalg.inv(M)
+    num_iter = 0
+    
+    for i in range(max_iter):
+        x_new = ( M_inv @ N ) @ x + M_inv @ b #itérations
+        num_iter += 1
+        
+        """conditions d'arrêt"""
+        if np.linalg.norm(x_new - x, ord = 2) < tol:
+            break
+        if num_iter == max_iter :
+            print("Nous avons atteint le nombre max d'itérations sans convergence")
+
+        x = x_new
+        
+    return [x, num_iter]
+
+
+#Méthode de Gauss-Seidel
+def gauss_seidel(A, b, x, max_iter, tol):
+    M = np.diag(np.diag(A)) + np.tril(A, k=-1)
+    N = - np.triu(A, k=1)
+    M_inv = np.linalg.inv(M)
+    num_iter = 0
+
+    for i in range(max_iter):
+        x_new = ( M_inv @ N ) @ x + M_inv @ b #itérations
+        num_iter += 1
+
+        """conditions d'arrêt"""
+        if np.linalg.norm(x_new - x, ord = 2) < tol:
+            break
+        if num_iter == max_iter :
+            print("Nous avons atteint le nombre max d'itérations sans convergence")
+
+        x = x_new
+        
+    return [x, num_iter]
+
+
+#Solutions
+"""Jacobi"""
+jac = jacobi(A, b, x, max_iter, tol)
+res = jac[0]
+it = jac[1]
+
+"""Gauss-Seidel"""
+G_S = gauss_seidel(A, b, x, max_iter, tol)
+resultat = G_S[0]
+nb_iter = G_S[1]
+
+#affichages
+print ("La matrice A :\n", A)
+print ("La matrice b :\n", b)
+print()
+print("le vecteur solution par Jacobi est : \n", res, "et est obtenu après ", it, " itérations")
+print()
+print("le vecteur solution par Gauss-Seidel est : \n", resultat, "et est obtenu après ", nb_iter, " itérations")
